c203ec6ddf72298bf2dcdae106ac2fa7e84d1d0e
mline03 <melinelevao13@gmail.com>
2025-09-10 14:40:26 -0600

    Complete Week 02 queue assignment: implemented TakingTurnsQueue and PriorityQueue with passing tests

--- week02/teach/Tests/PriorityQueue_Tests.cs
+++ week02/teach/Tests/PriorityQueue_Tests.cs
@@ -1,14 +1,33 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
-namespace Week02.Tests
+[TestClass]
+public class PriorityQueue_Tests
 {
-    [TestClass]
-    public class PriorityQueue_Tests
+    [TestMethod]
+    public void Enqueue_Dequeue_WorksCorrectly()
     {
-        [TestMethod]
-        public void AnotherSampleTest()
-        {
-            Assert.IsTrue(true);
-        }
+        var pq = new PriorityQueue<string>();
+        pq.Enqueue("low", 1);
+        pq.Enqueue("high1", 10);
+        pq.Enqueue("high2", 10);
+        pq.Enqueue("medium", 5);
+
+        // Dequeue should return "high1" first (highest priority, FIFO)
+        Assert.AreEqual("high1", pq.Dequeue());
+        // Then "high2"
+        Assert.AreEqual("high2", pq.Dequeue());
+        // Then "medium"
+        Assert.AreEqual("medium", pq.Dequeue());
+        // Then "low"
+        Assert.AreEqual("low", pq.Dequeue());
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void Dequeue_Throws_WhenEmpty()
+    {
+        var pq = new PriorityQueue<int>();
+        pq.Dequeue();
     }
 }
--- week02/teach/Tests/TakingTurnsQueue_Tests.cs
+++ week02/teach/Tests/TakingTurnsQueue_Tests.cs
@@ -1,14 +1,34 @@
-uusing Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
-namespace Week02.Tests
+[TestClass]
+public class TakingTurnsQueue_Tests
 {
-    [TestClass]
-    public class TakingTurnsQueue_Tests
+    [TestMethod]
+    public void AddPerson_And_GetNextPerson_WorksCorrectly()
     {
-        [TestMethod]
-        public void SampleTest()
-        {
-            Assert.AreEqual(1, 1);
-        }
+        var queue = new TakingTurnsQueue();
+        queue.AddPerson("Alice", 2);
+        queue.AddPerson("Bob", 0); // infinite turns
+
+        // First call returns Alice, turns decrease
+        Assert.AreEqual("Alice", queue.GetNextPerson());
+
+        // Second call returns Bob, infinite turns so still in queue
+        Assert.AreEqual("Bob", queue.GetNextPerson());
+
+        // Third call returns Alice again (since 2 turns)
+        Assert.AreEqual("Alice", queue.GetNextPerson());
+
+        // Fourth call returns Bob again
+        Assert.AreEqual("Bob", queue.GetNextPerson());
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void GetNextPerson_Throws_WhenQueueEmpty()
+    {
+        var queue = new TakingTurnsQueue();
+        queue.GetNextPerson();  // Should throw exception
     }
 }
